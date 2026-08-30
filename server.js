2d1f1534c077cf6b16fc821215a89fd81fb6df3f
jigyasukumar12rmps@gmail.com <jigyasukumar12@gmail.com>
2026-08-30 21:21:48 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+require("dotenv").config({path: ["./src/.env"]});
+
+
+const app = require("./src/app.js");
+const connectToDB = require("./src/config/db.js"); 
+
+connectToDB();
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
